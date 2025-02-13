fff2f80a73b050601976c277405092edcac80823
Julian.Jurkoic <Julian.Jurkoic@PointClickCare.com>
2025-02-13 09:18:45 -0700

    capitals

--- mysql-init/init.sql
+++ mysql-init/init.sql
@@ -6,17 +6,17 @@ CREATE TABLE users (
     email VARCHAR(100) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
+ 
 -- Insert 10 fake rows into the 'users' table
 INSERT INTO users (first_name, last_name, email)
 VALUES
-    ('John', 'Doe', 'john.doe@example.com'),
-    ('Jane', 'Smith', 'jane.smith@example.com'),
-    ('Alice', 'Johnson', 'alice.johnson@example.com'),
-    ('Bob', 'Brown', 'bob.brown@example.com'),
-    ('Charlie', 'Davis', 'charlie.davis@example.com'),
-    ('Dana', 'Miller', 'dana.miller@example.com'),
-    ('Eve', 'Wilson', 'eve.wilson@example.com'),
-    ('Frank', 'Taylor', 'frank.taylor@example.com'),
-    ('Grace', 'Moore', 'grace.moore@example.com'),
-    ('Hank', 'Anderson', 'hank.anderson@example.com');
+    ('John', 'Doe', 'John.Doe@example.com'),
+    ('Jane', 'Smith', 'JANE.smith@SPACEX.com'),
+    ('Alice', 'Johnson', 'alice.Johnson@example.COM'),
+    ('Bob', 'Brown', 'BOB.brown@example.com'),
+    ('Charlie', 'Davis', 'CHARLIE.DAVIS@example.com'),
+    ('Dana', 'Miller', 'dana.Miller@example.com'),
+    ('Eve', 'Wilson', 'EVE.wilson@example.com'),
+    ('Frank', 'Taylor', 'frank.taylor@SPACE.com'),
+    ('Grace', 'Moore', 'Grace.Moore@example.com'),
+    ('Hank', 'Anderson', 'Hank.anderson@example.com');
